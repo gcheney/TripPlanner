d588a32ccdccdc2a3545a9eca62189ac424f6a66
Glendon Cheney <glendon.cheney@gmail.com>
2016-11-17 12:54:09 -0600

    Add mail service

--- Startup.cs
+++ Startup.cs
@@ -8,13 +8,40 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using TripPlanner.Services;
 
 namespace TripPlanner
 {
     public class Startup
     {
+        private IHostingEnvironment _env;
+        private IConfigurationRoot _config;
+
+        public Startup(IHostingEnvironment env) 
+        {
+            _env = env;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(_env.ContentRootPath)
+                .AddJsonFile("config.json");
+
+            _config = builder.Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(_config);
+
+            if (_env.IsEnvironment("Development") || _env.IsEnvironment("Testing"))
+            {
+                services.AddScoped<IMailService, DebugMailService>();
+            }
+            else 
+            {
+                // use actual mail service
+                services.AddScoped<IMailService, DebugMailService>();
+            }
             services.AddMvc();
         }
 
@@ -33,7 +60,7 @@ namespace TripPlanner
                 config.MapRoute(
                     name: "Default",
                     template: "{controller}/{action}/{id?}",
-                    defaults: new { controller = "App", action = "Index" }
+                    defaults: new { controller = "Home", action = "Index" }
                 );
             });
         }
