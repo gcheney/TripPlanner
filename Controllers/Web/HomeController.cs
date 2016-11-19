54c498e262936d0f50c4da962cce56fd12b1f9c8
Glendon Cheney <glendon.cheney@gmail.com>
2016-11-19 15:25:55 -0600

    Redo layout with bootstrap

--- Controllers/Web/HomeController.cs
+++ Controllers/Web/HomeController.cs
@@ -42,11 +42,11 @@ namespace TripPlanner.Controllers.Web
       if (ModelState.IsValid)
       {
         _mailService.SendMail(_config["MailSettings:ToAddress"], 
-          model.Email, "From TheWorld", model.Message);
+          model.Email, "From Trip Planner", model.Message);
 
         ModelState.Clear();
 
-        ViewBag.UserMessage = "Message Sent";
+        ViewBag.UserMessage = "Message Sent!";
       }
 
       return View();
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,30 +1,16 @@
 // site.js
 (function () {
+  var $sidebarAndWrapper = $("#sidebar,#wrapper");
+  var $icon = $("#sidebarToggle i.fa");
 
-  /*
-  var main = $("#main");
-  main.on("mouseenter", function () {
-    main.css("background-color", "#888");
-  });
-  main.on("mouseleave", function () {
-    main.css("background-color", "");
-  });
-
-  $(".menu li a").on("click", function () {
-    alert($(this).text());
-    return false;
-  });
-  */
-
-  var $sidebarAndWrapper = $("#sidebar, #wrapper");
-
-  $("#menuToggle").on("click", function () {
+  $("#sidebarToggle").on("click", function () {
     $sidebarAndWrapper.toggleClass("hide-sidebar");
     if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
-      $(this).text("Show Menu");
+      $icon.removeClass("fa-angle-left");
+      $icon.addClass("fa-angle-right");
     } else {
-      $(this).text("Hide Menu");
+      $icon.removeClass("fa-angle-right");
+      $icon.addClass("fa-angle-left");
     }
   });
-
 })();
